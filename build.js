525ce2687f18f241ee5346c30cf11215c2923dc1
sophiajay <sophiajturner@outlook.com>
2025-12-01 21:25:34 +0000

    add ivy page

--- build.js
+++ build.js
@@ -9,5 +9,15 @@ const template = await fs.readFile(`${templateDir}/template.html`, "utf8");
 files.filter(f => f.endsWith("partial.html")).map(async f => {
     const content = await fs.readFile(`${templateDir}/${f}`, "utf8");
     const html = template.replace("%%CONTENT%%", content);
-    return fs.writeFile(`${siteDir}/${f.replace(".partial", "")}`, html);
+
+    if(f === "index.partial.html") {
+        console.log(`Building main page...`);
+        fs.writeFile(`${siteDir}/index.html`, html);
+    } else {
+        console.log(`Building page: ${f}...`);
+        const path = `${siteDir}/pages/${f.replace(".partial.html", "")}/index.html`
+        fs.mkdir(path.substring(0, path.lastIndexOf("/")), { recursive: true });
+        fs.writeFile(path, html);
+
+    }
 });
